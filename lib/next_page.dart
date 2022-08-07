3946f3ffa164b5c2949775d3b81faebabd970af8
RYOTA-F <ry.fj0304@gmail.com>
2022-08-07 16:30:25 +0900

    [update] NextPage スタイル調整

--- lib/next_page.dart
+++ lib/next_page.dart
@@ -6,7 +6,26 @@ class NextPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-    );
+        appBar: AppBar(),
+        body: Padding(
+            padding: const EdgeInsets.all(50.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: const [
+                    SizedBox(width: 30, child: Text('ID')),
+                    Text(':　毎回同じIDを表示'),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  children: const [
+                    SizedBox(width: 30, child: Text('PW')),
+                    Text(':　毎回同じPWを表示'),
+                  ],
+                ),
+              ],
+            )));
   }
 }
